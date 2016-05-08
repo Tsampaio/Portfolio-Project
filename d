7dda573e2d0238b7218212299d54804f53446b87
Telmo <telmoasampaio@gmail.com>
2016-05-08 20:24:00 +0100

    forEach loop added

---
+++ d
@@ -0,0 +1,29 @@
+[33mcommit f680f6e45c253ce98d6ef9541ef252252bc7cd52[m
+Author: Telmo <telmoasampaio@gmail.com>
+Date:   Sat May 7 21:14:57 2016 +0100
+
+    Map Div working
+
+[33mcommit 27b59448c991fb4ac429a053b04cf4d0a7d5169a[m
+Author: Telmo <telmoasampaio@gmail.com>
+Date:   Wed May 4 20:05:12 2016 +0100
+
+    icons added
+
+[33mcommit b65760fb0fee9e2fe645733e4adc5698c7384199[m
+Author: Telmo <telmoasampaio@gmail.com>
+Date:   Mon May 2 20:21:20 2016 +0100
+
+    Project1 added
+
+[33mcommit e30f00c484075af94795dfed92aa8d5489d59188[m
+Author: Telmo <telmoasampaio@gmail.com>
+Date:   Sun May 1 21:05:29 2016 +0100
+
+    Page structure added
+
+[33mcommit 292536e13c00798e5d3753ddb03f49f122f4865c[m
+Author: Telmo <telmoasampaio@gmail.com>
+Date:   Fri Apr 29 16:48:15 2016 +0100
+
+    Starting project
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,8 @@
 var bio = {
 	"name" : "Telmo Sampaio",
 	"role" : "Front End Developer",
-	"welcomeMessage" : "I'm a student from the Udacity Nanodegree Front End Developement. This is my online resume project where you can find some information about me and the projects that I'm currently working on.", 
+	"welcomeMessage" : "I'm a student from the Udacity Nanodegree Front End Developement. "
+	+ "This is my online resume project where you can find some information about me and the projects that I'm currently working on.",
 	"biopic" : "images/ts2.jpg",
 	"contacts" : {
 		"mobile" : "07852726903",
@@ -9,39 +10,39 @@ var bio = {
 		"github" : "https://github.com/Tsampaio",
 		"location" : "Manchester United Kingdom",
 		"skype" : "telmo.sampaio"
-	},	
+	},
 	"skills" : ["HTML","CSS","JQUERY","PHP"]
 };
 
 var work = {
 	"jobs" : [
 	{
-		"employer" : "Udacity", 
+		"employer" : "Udacity",
 		"title" : "Web-developer",
 		"dates" : "2016",
 		"location" : "Online",
 		"description" : "Develop and mantain responsive websites"
 	},
 	{
-		"employer" : "Pret A Manger", 
+		"employer" : "Pret A Manger",
 		"title" : "Team Leader",
 		"dates" : "2013 - 2016",
 		"location" : "Manchester",
-		"description" : "Responsible to manage stock, balance waste and Increase sales"	
+		"description" : "Responsible to manage stock, balance waste and Increase sales"
 	}]
 };
 
 var projects = {
 	"projects" : [{
 		"title" : "Portfolio Website",
-		"dates" : "2016", 
-		"description" : "Build a professional and responsive Portfolio Website", 
+		"dates" : "2016",
+		"description" : "Build a professional and responsive Portfolio Website",
 		"images" : ["images/project1.jpg"]
 	},
 	{
 		"title" : "Online Resume",
-		"dates" : "2016", 
-		"description" : "Build a professional and responsive Portfolio Website", 
+		"dates" : "2016",
+		"description" : "Build a professional and responsive Portfolio Website",
 		"images" : ["images/project2.jpg"]
 	}]
 };
@@ -51,7 +52,7 @@ var education = {
 	{
 		"name" : "University of Coimbra",
 		"location" : "Coimbra",
-		"degree" : "Masters", 
+		"degree" : "Masters",
 		"majors" : ["Civil Engineering"],
 		"dates" : "2005 - 2013",
 		"url" : "http://www.dec.uc.pt"
@@ -62,13 +63,13 @@ var education = {
 	{
 		"title" : "Front-End Web Developer Nanodegree",
 		"school" : "Udacity",
-		"dates" : "2016",
+		"date" : "2016",
 		"url" : "https://www.udacity.com"
 	},
 	{
 		"title" : "Front-End Web Developer",
 		"school" : "Teamtreehouse",
-		"dates" : "2015",
+		"date" : "2015",
 		"url" : "https://www.teamtreehouse.com"
 	}
 	]
@@ -80,21 +81,16 @@ bio.display = function() {
 	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 	var formattedImage = HTMLbioPic.replace("%data%",bio.biopic);
 	var formattedMessage = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
-	
 
 	$("#header").prepend(formattedImage).append(formattedName,formattedRole);
 	$(".about-me").append(formattedMessage);
-	
 
-	 for (var i = 0;  i < bio.skills.length; i++) {
-       	var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
+	bio.skills.forEach( function(skill) {
+    	var formattedSkills = HTMLskills.replace("%data%",skill);
 		$(".skills").append(formattedSkills);
-   }
+   });
 
-	
-
-	
-       	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+    	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
 		var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
 		var formattedSkype = HTMLcontactGeneric.replace("%contact%","skype: ").replace("%data%",bio.contacts.skype);
 		var formattedGitHub = HTMLgithub.replace("%data%",bio.contacts.github);
@@ -103,12 +99,11 @@ bio.display = function() {
    		$(".contacts").append(formattedMobile,formattedEmail,formattedSkype,formattedGitHub,formattedLocation);
 };
 
+
 education.display = function() {
 
 	$("#education").append(HTMLschoolStart);
 	$("#education").append(HTMLonlineClasses);
-
-
 
 	for (var i=0; i < education.schools.length; i++) {
         var formattedName = HTMLschoolName.replace("%data%",education.schools[i].name);
@@ -119,40 +114,32 @@ education.display = function() {
 		$(".education-entry:last").append(formattedName + formattedDegree,formattedDates,formattedLocation,formattedMajor);
    }
 
-	
-
 	for (i=0; i < education.onlineCourses.length; i++) {
-		
-		
+
 		var onlineName = HTMLonlineSchool.replace("%data%",education.onlineCourses[i].school);
 		var onlineDegree = HTMLonlineTitle.replace("%data%",education.onlineCourses[i].title);
 		var onlinedDates = HTMLonlineDates.replace("%data%",education.onlineCourses[i].dates);
 		var onlineUrl = HTMLonlineURL.replace("%data%",education.onlineCourses[i].url);
-		
+
 		$("#education").append(onlineDegree + onlineName, onlinedDates, onlineUrl);
 	}
 };
 
 
 work.display = function() {
-	
+
 	$("#workExperience").append(HTMLworkStart);
 
-		for (var i=0; i < work.jobs.length; i++) {
-	       	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
-			var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
-			var formattedDates = HTMLworkDates.replace("%data%",work.jobs[i].dates);
-			var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[i].description);
-			var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[i].location);
+	for (var i=0; i < work.jobs.length; i++) {
+       	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
+		var formattedDates = HTMLworkDates.replace("%data%",work.jobs[i].dates);
+		var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[i].description);
+		var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[i].location);
 
-			$(".work-entry:last").append(formattedEmployer + formattedTitle,formattedDates,formattedDescription, formattedLocation);
-		}
-
-		
-
-		
-   };
-
+		$(".work-entry:last").append(formattedEmployer + formattedTitle,formattedDates,formattedDescription, formattedLocation);
+	}
+};
 
 
 projects.display = function(){
@@ -164,12 +151,9 @@ projects.display = function(){
 		var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[i].title);
 		var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[i].dates);
 		var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[i].description);
-		
-		
 		var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[i].images[0]);
-		
 
-		$(".project-entry:last").append(formattedImage, formattedTitle,formattedDates,formattedDescription);	
+		$(".project-entry:last").append(formattedImage, formattedTitle,formattedDates,formattedDescription);
 	}
 
 };
@@ -178,7 +162,7 @@ projects.display = function(){
 function inName(name){
 	console.log(name);
 	var newName = name;
-	newName = newName[0].toUpperCase() + newName.slice(1,newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase(); 
+	newName = newName[0].toUpperCase() + newName.slice(1,newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase();
 
 	return newName;
 }
